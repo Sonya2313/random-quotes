a332d30a9c06051f0d2585620484d00378b39ae2
Sonya <sonyatrusevich@gmail.com>
2026-04-19 15:49:57 +0300

    Show quote and author separatyle and modified quotes array of strings to array of objects

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const quotes = [
-  'The only way to do great work is to love what you do. - Steve Jobs',
-  'Innovation distinguishes between a leader and a follower. - Steve Jobs',
-  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+  {
+    quote: 'The only way to do great work is to love what you do.',
+    author: 'Steve ',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: ' Jobs',
+  },
 ];
 
 const quoteElement = document.getElementById('quote');
+const quoteAuthorElement = document.getElementById('quote-author');
 const generateBtn = document.getElementById('generate-btn');
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  console.log(randomIndex);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  const quote = randomQuote.quote;
+  const quoteAuthor = randomQuote.author;
+
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = quoteAuthor;
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
