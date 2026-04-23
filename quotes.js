f548c211978a8c7e5d02d6c00c24d1298b2f9b9b
Sonya <sonyatrusevich@gmail.com>
2026-04-23 18:04:59 +0300

    show favorite quotes

--- quotes.js
+++ quotes.js
@@ -2,44 +2,54 @@ const quotes = [
   {
     quote: 'The only way to do great work is to love what you do.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: it is the courage to continue that counts.',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote:
       "Your time is limited, so don't waste it living someone else's life.",
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Whether you think you can, or you think you can’t—you’re right.',
     author: 'Henry Ford',
+    isFavorite: false,
   },
   {
     quote: 'It does not matter how slowly you go as long as you do not stop.',
     author: 'Confucius',
+    isFavorite: false,
   },
   {
     quote: 'Do what you can, with what you have, where you are.',
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'Action is the foundational key to all success.',
     author: 'Pablo Picasso',
+    isFavorite: false,
   },
   {
     quote: 'If you can dream it, you can do it.',
     author: 'Walt Disney',
+    isFavorite: false,
   },
   {
     quote: 'Simplicity is the soul of efficiency.',
     author: 'Austin Freeman',
+    isFavorite: false,
   },
   {
     quote: 'Make it work, make it right, make it fast.',
     author: 'Kent Beck',
+    isFavorite: false,
   },
 ];
 
