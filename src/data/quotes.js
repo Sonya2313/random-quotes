f7b31a5187ec3167361ec1bdbdd24bb34ebd76c0
Sonya <sonyatrusevich@gmail.com>
2026-05-16 09:14:04 +0300

    Add quote id to each favorite quote

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,50 +1,60 @@
 const quotes = [
   {
+    id: '44c608e5',
     text: 'The only way to do great work is to love what you do.',
     author: 'Steve Jobs',
     isFavorite: false,
   },
   {
+    id: '4191fd32',
     text: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
     author: 'Winston Churchill',
     isFavorite: false,
   },
   {
+    id: 'b24b939a',
     text: "Your time is limited, so don't waste it living someone else's life.",
     author: 'Steve Jobs',
     isFavorite: false,
   },
   {
+    id: 'e1d5c965',
     text: 'Whether you think you can, or you think you can’t—you’re right.',
     author: 'Henry Ford',
     isFavorite: false,
   },
   {
+    id: '978de12f',
     text: 'It does not matter how slowly you go as long as you do not stop.',
     author: 'Confucius',
     isFavorite: false,
   },
   {
+    id: 'cb836071',
     text: 'Do what you can, with what you have, where you are.',
     author: 'Theodore Roosevelt',
     isFavorite: false,
   },
   {
+    id: '8b78b4ce',
     text: 'Action is the foundational key to all success.',
     author: 'Pablo Picasso',
     isFavorite: false,
   },
   {
+    id: '2908dced',
     text: 'If you can dream it, you can do it.',
     author: 'Walt Disney',
     isFavorite: false,
   },
   {
+    id: '735da634',
     text: 'Simplicity is the soul of efficiency.',
     author: 'Austin Freeman',
     isFavorite: false,
   },
   {
+    id: '5c6195d4',
     text: 'Make it work, make it right, make it fast.',
     author: 'Kent Beck',
     isFavorite: false,
--- src/handlers/favorite.js
+++ src/handlers/favorite.js
@@ -2,13 +2,12 @@ import { favoriteBtn } from '../../index.js';
 
 function toggleFavorite(quote, btn, container) {
   quote.isFavorite = !quote.isFavorite;
-  const { text, author, isFavorite } = quote;
-  toggleFavoriteBtnIcon(isFavorite, btn);
+  toggleFavoriteBtnIcon(quote.isFavorite, btn);
 
-  if (isFavorite) {
-    showFavoriteCard(text, author, container);
+  if (quote.isFavorite) {
+    showFavoriteCard(quote, container);
   } else {
-    hideFavoriteCard(text);
+    hideFavoriteCard(quote.id);
   }
 }
 
@@ -30,19 +29,12 @@ function hideFavoriteBtn(btn) {
   btn.style.display = 'none';
 }
 
-function showFavoriteCard(text, author, container) {
-  const favoriteCards = document.querySelectorAll('.favorite-card');
-
-  for (const card of favoriteCards) {
-    if (card.dataset.quote === quote) {
-      return;
-    }
-  }
-
+function showFavoriteCard(quote, container) {
+  const { id, text, author } = quote;
   const favoriteCard = document.createElement('div');
   favoriteCard.classList.add('favorite-card');
   favoriteCard.dataset.quote = quote;
-
+  favoriteCard.dataset.quoteId = id;
   favoriteCard.innerHTML = `
     <p>${text}</p>
     <p class="author">${author}</p>
@@ -51,14 +43,14 @@ function showFavoriteCard(text, author, container) {
   container.appendChild(favoriteCard);
 }
 
-function hideFavoriteCard(text) {
-  const favoriteCards = document.querySelectorAll('.favorite-card');
+function hideFavoriteCard(id) {
+  const card = document.querySelectorAll(
+    `.favorite-card[data-quote-id="${id}"]`,
+  );
 
-  favoriteCards.forEach((card) => {
-    if (card.textContent.includes(text)) {
-      card.remove();
-    }
-  });
+  if (card) {
+    card.remove();
+  }
 }
 
 export { handleFavorite, toggleFavorite, hideFavoriteBtn };
